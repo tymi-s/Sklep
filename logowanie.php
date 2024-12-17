05641fc26a15af7c2da50b866979a1e7cb2fdb80
Gaba <gabi.wojcik2008@gmail.com>
2024-12-17 12:07:04 +0100

    style

--- logowanie.php
+++ logowanie.php
@@ -9,7 +9,7 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Rejestracja i logowanie</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="rejestracja.css">
     </head>
     <body>
         <div class="container" id ="logowanie">
