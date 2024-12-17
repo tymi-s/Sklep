64ed9ea0e826fbfe5202db0d0807ae99826cc525
nck23 <bartekszmyd1337@gmail.com>
2024-12-17 19:28:14 +0100

    zmiana nazwy style dla glownego

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIGMA | Sklep</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body> 
     <div class="container">
